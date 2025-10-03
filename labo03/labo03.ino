c4999c1874ee456d076d8085ab65ba1c3a828d31
cuzimapanda2003 <marcblais03@gmail.com>
2025-10-03 15:46:44 -0400

    Update labo03.ino

--- labo03/labo03.ino
+++ labo03/labo03.ino
@@ -1,62 +1,46 @@
 #include <MeAuriga.h>
 
 int stateCount = 0;
-
-int nbTuilAvanceCour = 3;
-int nbTuilAvanceLong = 5;
-
-int distLong = 30.48 * nbTuilAvanceLong;
+int nbTuilAvanceCour = 1;
+int nbTuilAvanceLong = 1;
 int distCour = 30.48 * nbTuilAvanceCour;
+int distLong = 30.48 * nbTuilAvanceLong;
 
-enum State {
-  avancePetit,
-  avanceGrand,
-  tourne,
-  stop
-};
-
+enum State { avancePetit, avanceGrand, tourne, stop };
 State state = stop;
-
 
 MeEncoderOnBoard encoderLeft(SLOT1);
 MeEncoderOnBoard encoderRight(SLOT2);
+MeGyro gyro(0, 0x69);
 
+#define LEDNUM 12
+#define LEDPIN 44
+MeRGBLed led(PORT0, LEDNUM);
 
-MeGyro gyro(PORT0, 0x69);
+long currentTime = 0;
+long previousTime = 0;
+long delayTime = 3000;
 
-#define LEDNUM  12
-#define LEDPIN  44
-#define RINGALLLEDS 0
+bool turning = false;
+float targetAngle = 0;
 
-MeRGBLed led( PORT0, LEDNUM );
-
-
-void isr_process_encoder1(void) {
-  if (digitalRead(encoderLeft.getPortB()) == 0) {
-    encoderLeft.pulsePosMinus();
-  } else {
-    encoderLeft.pulsePosPlus();
-  }
+void isr_process_encoder1() {
+  if (digitalRead(encoderLeft.getPortB()) == 0) encoderLeft.pulsePosMinus();
+  else encoderLeft.pulsePosPlus();
 }
 
-
-void isr_process_encoder2(void) {
-  if (digitalRead(encoderRight.getPortB()) == 0) {
-    encoderRight.pulsePosMinus();
-  } else {
-    encoderRight.pulsePosPlus();
-  }
+void isr_process_encoder2() {
+  if (digitalRead(encoderRight.getPortB()) == 0) encoderRight.pulsePosMinus();
+  else encoderRight.pulsePosPlus();
 }
 
 void setup() {
+  led.setpin(LEDPIN);
   attachInterrupt(encoderLeft.getIntNum(), isr_process_encoder1, RISING);
   attachInterrupt(encoderRight.getIntNum(), isr_process_encoder2, RISING);
-
   Serial.begin(115200);
   gyro.begin();
   gyro.resetData();
-
-
   encoderLeft.setPulse(9);
   encoderRight.setPulse(9);
   encoderLeft.setRatio(39.267);
@@ -65,147 +49,98 @@ void setup() {
   encoderRight.setPosPid(1.8, 0, 1.2);
   encoderLeft.setSpeedPid(0.18, 0, 0);
   encoderRight.setSpeedPid(0.18, 0, 0);
-
-
 }
-
-
-
 
 void keepStraight(short speed = 100, bool firstRun = false) {
   static double zAngleGoal = 0.0;
   static double previousError = 0.0;
-
   const double kp = 3.0;
   const double kd = 1.0;
-
   if (firstRun) {
-    gyro.resetData();
     zAngleGoal = gyro.getAngleZ();
     previousError = 0;
-    Serial.println("Direction locked.");
     return;
   }
-
   double error = gyro.getAngleZ() - zAngleGoal;
   double output = kp * error + kd * (error - previousError);
   previousError = error;
-
   encoderLeft.setTarPWM(speed - output);
   encoderRight.setTarPWM(-speed - output);
 }
 
-
-
 void avancerCm(int distanceCm, short speed = 100) {
   long startL = encoderLeft.getCurPos();
   long startR = encoderRight.getCurPos();
-
-  long targetTicks = (long)(distanceCm * 10.0 * 39.267);
-
+  long targetTicks = (long)(distanceCm * 39.267);
   bool done = false;
-  keepStraight(speed, true); 
-
+  keepStraight(-speed, true);
   while (!done) {
     encoderLeft.loop();
     encoderRight.loop();
-
-    keepStraight(speed); 
-
+    keepStraight(-speed);
     long posL = abs(encoderLeft.getCurPos() - startL);
     long posR = abs(encoderRight.getCurPos() - startR);
     long avgPos = (posL + posR) / 2;
-
-    if (avgPos >= targetTicks) {
-      done = true;
-    }
+    if (avgPos >= targetTicks) done = true;
   }
-
   encoderLeft.setTarPWM(0);
   encoderRight.setTarPWM(0);
 }
 
 
-
 void loop() {
+  currentTime = millis();
   encoderLeft.loop();
   encoderRight.loop();
-
   stateMachine();
 }
 
-
-
-void stateMachine(){
-
-switch (state) {
+void stateMachine() {
+  switch (state) {
+    case stop:
+      ledTaskRed();
+      if (pauseStop()) {
+        stateCount = 0;
+        state = avancePetit;
+      }
+      break;
     case avancePetit:
       ledTaskVert();
       avancerCm(distCour, 100);
       state = tourne;
       break;
-
     case avanceGrand:
-    avancerCm(distLong, 100);
+      ledTaskVert();
+      avancerCm(distLong, 100);
       state = tourne;
       break;
-
     case tourne:
-    ledTaskBlue();
+      ledTaskBlue();
 
-
-      if(stateCount == 1 || stateCount == 3){
-        state = avancePetit;
         stateCount++;
-      }
-      if(stateCount == 0 || stateCount == 2){
-        state = avanceGrand;
-        stateCount++;
-      }
-      if(stateCount == 4){
-        state = stop;
-      }
-      break;
-      case stop:
-      ledTaskRed();
-
-
-      stateCount = 0;
+        if (stateCount == 1 || stateCount == 3) state = avanceGrand;
+        else if (stateCount == 2) state = avancePetit;
+        else if (stateCount == 4) {
+          state = stop;
+          previousTime = currentTime;
+        }
       break;
   }
-
-
 }
-
 
 void ledTaskVert() {
-  led.setColor(0, 0, 0, 0);
-  for (int i = 1; i <= LEDNUM; i++) {
-    led.setColor(i, 0, 255, 0);
-  }
-  led.show(); 
+  for (int i = 1; i <= LEDNUM; i++) led.setColor(i, 0, 255, 0);
+  led.show();
 }
 
-void ledTaskBlue(){
-  led.setColor(0, 0, 0, 0);
-  for (int i = 1; i <= LEDNUM; i++) {
-    led.setColor(i, 0, 0, 255);
-  }
-  led.show(); 
+void ledTaskBlue() {
+  for (int i = 1; i <= LEDNUM; i++) led.setColor(i, 0, 0, 255);
+  led.show();
 }
 
-void ledTaskRed(){
-  led.setColor(0, 0, 0, 0);
-  for (int i = 1; i <= LEDNUM; i++) {
-    led.setColor(i, 255, 0, 0);
-  }
-  led.show(); 
+void ledTaskRed() {
+  for (int i = 1; i <= LEDNUM; i++) led.setColor(i, 255, 0, 0);
+  led.show();
 }
 
-
-void pauseStop() {
-
-}
-
-
-
+bool pauseStop() { return (millis() - previousTime >= delayTime); }
